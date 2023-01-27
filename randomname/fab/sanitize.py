9e4497bd4bda20e6acce903ce37a353c85a1a6f3
Bea Steers <bea.steers@gmail.com>
2023-01-27 00:41:06 -0500

    clean up fab scripts

--- randomname/fab/sanitize.py
+++ randomname/fab/sanitize.py
@@ -8,12 +8,12 @@ import randomname
 
 INVALID_PATTERN = re.compile('[\W_]+')
 def _sanitize(word):
-    return INVALID_PATTERN.sub('', word).lower()
+    return INVALID_PATTERN.sub('', word.split('(')[0]).lower()
 
 def sanitize(root, dry_run=False):
     for f in glob.glob(os.path.join(root, '**/*.txt'), recursive=True):
         wl = randomname.WordListFile(f)
-        wl2 = randomname.WordList([_sanitize(w) for w in wl])
+        wl2 = randomname.WordList(randomname.util.unique([_sanitize(w) for w in wl]))
         diff = {w1: w2 for w1, w2 in zip(wl, wl2) if w1 != w2}
         if diff:
             print(f)
@@ -24,4 +24,4 @@ def sanitize(root, dry_run=False):
 
 if __name__ == '__main__':
     import fire
-    fire.Fire()
+    fire.Fire(sanitize)
