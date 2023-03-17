469e96a17eb066c6f5f61a348f9d7531aa583136
Leopold Haller <leo@agentic.ai>
2023-03-17 11:51:27 -0700

    Refactor tests into unittest.

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,30 +1,45 @@
 from pathlib import Path
 
 import randomname
+import tempfile
+
+import unittest
 
 
-def test_get_name():
-    name = randomname.get_name('music_theory', 'cats').split('-', 1)
-    assert len(name) > 1
-    assert name[0] in randomname.util.get_groups_list('a/music_theory')
-    assert name[1] in randomname.util.get_groups_list('n/cats')
-    assert 'asdf' in randomname.util.get_groups_list(['n/cats', 'asdf'])
+class TestCode(unittest.TestCase):
+    '''Tests the core module.'''
 
-def test_generate():
-    name = randomname.generate('n/cats', 'a/music_theory', 'n/food').split('-', 2)
-    assert len(name) > 2
-    assert name[0] in randomname.util.get_groups_list('n/cats')
-    assert name[1] in randomname.util.get_groups_list('a/music_theory')
-    assert name[2] in randomname.util.get_groups_list('n/food')
-    assert name[0] not in randomname.util.get_groups_list('n/food')
+    def test_get_name(self):
+        name = randomname.get_name('music_theory', 'cats').split('-', 1)
+        assert len(name) > 1
+        assert name[0] in randomname.util.get_groups_list('a/music_theory')
+        assert name[1] in randomname.util.get_groups_list('n/cats')
+        assert 'asdf' in randomname.util.get_groups_list(['n/cats', 'asdf'])
 
-def test_generate_with_seed():
-    for seed in range(0, 100, 10):
-        assert len({randomname.generate(seed=seed, sep='_') for _ in range(20)}) == 1
+    def test_generate(self):
+        name = randomname.generate('n/cats', 'a/music_theory', 'n/food').split('-', 2)
+        self.assertGreater(len(name), 2)
+        self.assertIn(name[0], randomname.util.get_groups_list('n/cats'))
+        self.assertIn(name[1], randomname.util.get_groups_list('a/music_theory'))
+        self.assertIn(name[2], randomname.util.get_groups_list('n/food'))
+        self.assertIn(name[0], randomname.util.get_groups_list('n/food'))
 
-def test_generated_names_are_valid_file_names(tmp_path: Path):
-    random_names = [randomname.generate() for _ in range(10)]
+    def test_generate_with_seed(self):
+        for seed in range(0, 100, 10):
+            with self.subTest(seed=seed):
+                self.assertEqual(
+                    [randomname.generate(seed=seed, sep='_')
+                     for _ in range(20)],
+                    [randomname.generate(seed=seed, sep='_')] * 20)
 
-    for name in random_names:
-        # does not raise
-        (Path(tmp_path) / name).touch()
+    def test_generated_names_are_valid_file_names(self):
+        with tempfile.TemporaryDirectory() as tmp_path:
+            random_names = [randomname.generate() for _ in range(10)]
+
+            for name in random_names:
+                # does not raise
+                (Path(tmp_path) / name).touch()
+
+
+if __name__ == '__main__':
+    unittest.main()
